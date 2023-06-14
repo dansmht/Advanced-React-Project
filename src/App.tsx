5beb8cdbe33cb9035e66ff1927b5f6b86ec1caac
Daniil Smakhtsin <dansmht16@gmail.com>
2023-06-14 19:37:50 +0300

    add themes with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 // import { AboutPage } from "./pages/AboutPage/AboutPage";
 // import { MainPage } from "./pages/MainPage/MainPage";
-import classes from './index.scss';
+import classes from "./styles/index.scss";
+import classNames from "classnames";
+import { ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
-const AboutPageAsync = lazy(() => import('./pages/AboutPage/AboutPage.lazy'))
-const MainPageAsync = lazy(() => import('./pages/MainPage/MainPage.lazy'))
+const AboutPageAsync = lazy(() => import("./pages/AboutPage/AboutPage.lazy"));
+const MainPageAsync = lazy(() => import("./pages/MainPage/MainPage.lazy"));
+
+
 
 export const App = () => {
+
+  const { theme, toggleTheme } = useTheme();
+
   return (
-      <div className={classes.App}>
-        <Link to='/'>Главная</Link>
-        <Link to='/about'>О сайте</Link>
+      <div className={classNames(classes.App, classes[theme])}>
+
+        <button onClick={toggleTheme}>TOGGLE</button>
+
+        <Link to="/">Главная</Link>
+        <Link to="/about">О сайте</Link>
 
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-              <Route path='/about' element={<AboutPageAsync />} />
-              <Route path='/' element={<MainPageAsync />} />
+              <Route path="/about" element={<AboutPageAsync />} />
+              <Route path="/" element={<MainPageAsync />} />
           </Routes>
         </Suspense>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import {render} from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./App";
+import ThemeProvider from "./theme/ThemeProvider";
 
 render(
   <Router>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Router>,
   document.getElementById('root'),
 );
