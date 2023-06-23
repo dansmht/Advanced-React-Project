71eb38f96ec988debf0a821ed5c1b1aacfbbdb96
Daniil Smakhtsin <dansmht16@gmail.com>
2023-06-23 18:01:53 +0300

    fix ThemeDecorator for storybook

--- src/shared/config/storybook/decorators/ThemeDecorator.tsx
+++ src/shared/config/storybook/decorators/ThemeDecorator.tsx
@@ -1,30 +1,31 @@
-import { useMemo } from "react";
+import { useLayoutEffect } from "react";
 import { Decorator } from "@storybook/react";
 import { addons } from "@storybook/preview-api";
 import { UPDATE_GLOBALS } from "@storybook/core-events";
 import { Theme } from "app/providers";
 import { ThemeContext } from "app/providers/ThemeProvider/lib/ThemeContext";
-import classes from "../../../../app/styles/index.scss";
+import classes from "../../../../app/App.scss";
 
 export const ThemeDecorator: Decorator = (Story, context) => {
 
   const theme = context?.globals?.theme as Theme || Theme.LIGHT;
 
-  const providerProps = useMemo(() => ({
-    theme,
-    setTheme: (newTheme: Theme) => {
-      context.globals.theme = newTheme;
-      addons.getChannel().emit(UPDATE_GLOBALS, {
-        globals: {
-          theme: newTheme,
-        }
-      });
-    }
-  }), [theme]);
+  const setTheme = (newTheme: Theme) => {
+    context.globals.theme = newTheme;
+    addons.getChannel().emit(UPDATE_GLOBALS, {
+      globals: {
+        theme: newTheme,
+      }
+    });
+  };
+
+  useLayoutEffect(() => {
+    document.documentElement.className = theme;
+  }, []);
 
   return (
-    <ThemeContext.Provider value={providerProps}>
-      <div style={{minHeight: "auto"}} className={`${classes.App} ${classes[theme]}`}>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div style={{ minHeight: "auto" }} className={classes.App}>
         <Story />
       </div>
     </ThemeContext.Provider>
