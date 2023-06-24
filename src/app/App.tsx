7548532e46315814a0ba8418e9b786123b262b72
Daniil Smakhtsin <dansmht16@gmail.com>
2023-06-25 01:47:24 +0300

    remove modal stub from app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,28 +2,22 @@ import { Suspense } from "react";
 import classNames from "classnames";
 import { AppRouter, useTheme } from "app/providers";
 import { NavBar, SideBar } from "widgets";
-import { Button, Loader } from "shared/ui";
+import { Loader } from "shared/ui";
 import classes from "./App.scss";
-import { Modal } from "shared/ui/Modal/Modal";
-import { useModal } from "shared/lib/hooks/useModal";
 
 export const App = () => {
 
   useTheme();
 
-  const {isOpen,onClose, onOpen} = useModal();
-
   return (
     <Suspense fallback={<Loader />}>
       <div className={classNames(classes.App)}>
         <NavBar />
-        <Button onClick={onOpen}>Open</Button>
 
         <div className={classes.Content}>
           <SideBar />
           <AppRouter />
         </div>
-        <Modal isOpen={isOpen} title="Auth" onClose={onClose} />
       </div>
     </Suspense>
   );
