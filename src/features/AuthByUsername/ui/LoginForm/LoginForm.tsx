29ac6a9c7cc83712d7c1a9874e5f533b5c529cf5
Daniil Smakhtsin <dansmht16@gmail.com>
2023-06-30 16:00:44 +0300

    use InputGroup component in LoginForm & use useInput hook to make controlled inputs

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,7 +1,8 @@
 import { FC, ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import classNames from "classnames";
-import { Button, Input } from "shared/ui";
+import { Button, Input, InputGroup, Label } from "shared/ui";
+import { useInput } from "shared/lib/hooks";
 import classes from "./LoginForm.scss";
 
 interface LoginFormProps {
@@ -13,13 +14,35 @@ export const LoginForm: FC<LoginFormProps> = ({ className }) => {
 
   const { t } = useTranslation();
 
+  const [username, setUsername] = useInput();
+  const [password, setPassword] = useInput();
+
   return (
     <form className={classNames(classes.LoginForm, className)}>
-      <Input placeholder="Username" variant="unstyled" />
-      <Input placeholder="Password" variant="flushed" leftAdornment={<div>Q</div>} rightAdornment={<div>QWE</div>} />
-      <Input placeholder="Password 2" variant="filled" />
-      <Input placeholder="Password 3" variant="outline" />
-      <Button>
+      <InputGroup>
+        <Label>{t("username")}</Label>
+        <Input
+          variant="flushed"
+          placeholder={t("typeUsername")}
+          name="username"
+          value={username}
+          onChange={setUsername}
+        />
+      </InputGroup>
+
+      <InputGroup>
+        <Label>{t("password")}</Label>
+        <Input
+          variant="flushed"
+          type="password"
+          placeholder={t("typePassword")}
+          name="password"
+          value={password}
+          onChange={setPassword}
+        />
+      </InputGroup>
+
+      <Button type="submit">
         {t("login")}
       </Button>
     </form>
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,8 @@
 export { AppLink, AppLinkTheme } from "./AppLink/AppLink";
 export { Button, ButtonTheme } from "./Button/Button";
-export { Input, InputVariant } from "./Input/Input";
+export { InputGroup } from "./Input/InputGroup/InputGroup";
+export { Label } from "./Input/Label/Label";
+export { Input, InputVariant } from "./Input/Input/Input";
 export { Loader } from "./Loader/Loader";
 export { Modal } from "./Modal/Modal";
 export { Portal } from "./Portal/Portal";
