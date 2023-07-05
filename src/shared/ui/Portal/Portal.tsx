77cdab247da9e3f5f795b3ad02a8052d18738029
Daniil Smakhtsin <dansmht16@gmail.com>
2023-07-05 21:05:36 +0300

    change Portal component to append children to new div element

--- src/shared/ui/Portal/Portal.tsx
+++ src/shared/ui/Portal/Portal.tsx
@@ -1,14 +1,19 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 interface PortalProps {
   children: ReactNode;
-  container?: Element;
 }
 
-export const Portal: FC<PortalProps> = ({
-  children,
-  container = document.body,
-}) => {
+export const Portal: FC<PortalProps> = ({ children }) => {
+  const [container] = useState(() => document.createElement("div"));
+
+  useEffect(() => {
+    document.body.appendChild(container);
+    return () => {
+      document.body.removeChild(container);
+    };
+  }, [container]);
+
   return createPortal(children, container);
 };
